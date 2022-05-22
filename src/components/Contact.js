d5638010655bba09a3bd56d89a38b2a189b5ee2b
Jaxon Adams <jadamsresume1@gmail.com>
2022-05-21 21:24:49 -0600

    converted form to controlled component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Contact = () => {
+    const [ formState, setFormState ] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+        // submission to server handled here
+    };
+
     return (
         <section>
             <h1>Contact Me</h1>
-            <form id='contact-form'>
+            <form id='contact-form' onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor='name'>Name:</label>
-                    <input type='text' name='name' />
+                    <input type='text' name='name' defaultValue={name} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor='email'>Email:</label>
-                    <input type='email' name='email' />
+                    <input type='email' name='email' defaultValue={email} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor='message'>Message:</label>
-                    <textarea name='message' rows='5' />
+                    <textarea name='message' rows='5' defaultValue={message} onChange={handleChange} />
                 </div>
                 <button type='submit'>Submit</button>
             </form>
